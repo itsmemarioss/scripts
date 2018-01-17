ff7bf2309ead161a33702e3e197817ace4b932d7
Mário Santos <super.mario.santos.sousa@gmail.com>
2018-01-17 12:44:11 -0300

    shift

--- highest.sh
+++ highest.sh
@@ -1,15 +1,34 @@
+order=-h
 filename=${1:?Please enter the file name}
-count=${2:-5}
+count=5
+usage="USAGE: $0 [-h|-l] [-n count] filename"
 
-if [ $count -lt 0 ]; then
-	echo "Pleas enter a valid count value"
+if [ $1 = '-h' ] || [ $1 = '-l' ]; then
+	order=$1
+	shift
 fi
-
-if [ -s $filename ]; then
-	sort -n $filename | tail -$count 2>/dev/null
+if [ $1 = '-n' ]; then
+	if [[ -n $(echo $2 | grep '^[1-9]$') ]]; then
+		count=$2
+		shift 2
+	else
+		echo "Invalid value for count"
+		echo $usage
+		exit 1
+	fi
+	count=$2
+	shift 2
+fi
+if [ -s $1 ]; then
+	filename=$1
 else
-	echo "Please enter a valid,non empty filename"
-	echo "Thanks"
+	echo "Invalid filename or file is empty"
+	echo $usage
 	exit 1
 fi
-
+case $order in
+	-h ) sort -n $filename | tail -$count ;;
+	-l ) sort -rn $filename | tail -$count ;;
+	* )
+	echo $usage
+esac
---
+++ shift.sh
@@ -0,0 +1,9 @@
+option=$1
+arg1=$2
+arg2=$3
+if [ $1 = '-l' ]; then
+	shift
+fi
+
+echo arg1: $1, arg2:$2
+
